cb2f104c82db94d7b8b7ceaa3543171cab096062
aayushpatidar027 <aayushpatidar027@gmail.com>
2026-05-19 15:19:06 +0530

    added ApplicationConstant class

--- src/main/java/com/example/sms/constant/ApplicationConstants.java
+++ src/main/java/com/example/sms/constant/ApplicationConstants.java
@@ -5,4 +5,6 @@ public class ApplicationConstants
     public static final String USER_ALREADY_EXIST = "User already exist with this email or username";
     public static final String USER_NOT_FOUND = "User not found with this id";
     public static final String ADDRESS_NOT_FOUND = "Address not found with this id";
+    public static final String USER_DELETED_SUCCESSFULLY = "User deleted successfully";
+    public static final String UNKNOWN_ERROR= "An unknown error occurred. Please try again later.";
 }
